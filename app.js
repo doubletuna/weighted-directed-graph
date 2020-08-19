023f1d5db75b982037ddc8dfdd842b4b46775f15
Danny <dannyl@spectory.com>
2020-08-19 17:56:24 +0300

    prod adjustments

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const routeRoutes = require('./routes/route')
 app.use(routeRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.listen(port, () => {
   console.log(`running at port ${port}`);
 });
